dc26bc6637da436f924396325013254ee1b9eb8e
Aaron Masover <aaron.masover@wtsparadigm.com>
2019-08-14 15:32:23 -0600

    few more changes

---
+++ .zsh_plugins.sh
@@ -0,0 +1,2 @@
+source /home/aaron/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm/zsh-nvm.plugin.zsh
+fpath+=( /home/aaron/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm )
